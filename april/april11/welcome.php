2391360fa7402a7f8835862d5ff916f64ffeb9b2
jeeva <54775784+jeeva-devhub@users.noreply.github.com>
2025-04-11 15:46:47 +0530

    Add files via upload

---
+++ april/april11/welcome.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>welcome</title>
+</head>
+<body>
+    welcome <?php echo $_POST["name"];?><br>
+    your email address is <?php echo $_POST["email"];?><br>
+    your mobile number is <?php echo $_POST["number"];?></br>
+    
+</body> 
+</html>
